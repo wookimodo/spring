7fb15eee6672c571df19a1f651ae622fa4880ec9
DESKTOP-0A9J2K9\KIM <rudghk1437@naver.com>
2023-03-21 11:38:42 +0900

    Adapter pattern

--- design/src/com/company/design/Main.java
+++ design/src/com/company/design/Main.java
@@ -1,5 +1,9 @@
 package com.company.design;
 
+import com.company.design.adapter.Cleaner;
+import com.company.design.adapter.Electronic110V;
+import com.company.design.adapter.HairDryer;
+import com.company.design.adapter.SocketAdapter;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
@@ -7,6 +11,7 @@ import com.company.design.singleton.SocketClient;
 public class Main {
     public static void main(String[] args) {
 
+        /*
         AClazz aClazz = new AClazz();
         BClazz bClazz = new BClazz();
         // AClazz, BClazz에서 SocketClient를 생성하는 것이 아니라, SocketClient 자신을 통해서만 생성되도록.
@@ -15,6 +20,17 @@ public class Main {
         // 그래서 true
         System.out.println("두 개의 객체가 동일한가?");
         System.out.println(aClient.equals(bClient));
+         */
 
+        HairDryer hairDryer = new HairDryer();
+        connect(hairDryer);
+
+        Cleaner cleaner = new Cleaner();
+        Electronic110V adapter = new SocketAdapter(cleaner);
+        connect(adapter);
+    }
+
+    public static void connect(Electronic110V electronic110V){
+        electronic110V.powerOn();
     }
 }
