cb06d9929516c94b15c5434f7ea069411e069a6b
DESKTOP-0A9J2K9\KIM <rudghk1437@naver.com>
2023-03-21 14:43:34 +0900

    proxy pattern

--- design/src/com/company/design/Main.java
+++ design/src/com/company/design/Main.java
@@ -4,9 +4,15 @@ import com.company.design.adapter.Cleaner;
 import com.company.design.adapter.Electronic110V;
 import com.company.design.adapter.HairDryer;
 import com.company.design.adapter.SocketAdapter;
+import com.company.design.aop.AopBrowser;
+import com.company.design.proxy.Browser;
+import com.company.design.proxy.BrowserProxy;
+import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,7 +26,7 @@ public class Main {
         // 그래서 true
         System.out.println("두 개의 객체가 동일한가?");
         System.out.println(aClient.equals(bClient));
-         */
+
 
         HairDryer hairDryer = new HairDryer();
         connect(hairDryer);
@@ -28,6 +34,37 @@ public class Main {
         Cleaner cleaner = new Cleaner();
         Electronic110V adapter = new SocketAdapter(cleaner);
         connect(adapter);
+
+
+        Browser browser = new Browser("www.naver.com");
+        browser.show();
+
+        IBrowser browser = new BrowserProxy("www.naver.com");
+        browser.show();
+        browser.show();
+        browser.show();
+        browser.show();
+
+         */
+
+        AtomicLong start = new AtomicLong();
+        AtomicLong end  = new AtomicLong();
+
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+        );
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
+
     }
 
     public static void connect(Electronic110V electronic110V){
