9cbb791093acc5ea79f7c17ada70275d9707d3f4
DESKTOP-0A9J2K9\KIM <rudghk1437@naver.com>
2023-03-28 11:35:17 +0900

    Validation

---
+++ validation/src/main/java/com/example/validation/controller/ApiController.java
@@ -0,0 +1,41 @@
+package com.example.validation.controller;
+
+import com.example.validation.dto.User;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    // user를 request받아서 user를 return하는 에코 형태
+    // @Valid를 붙여서 annotation이 달린 객체를 검증.
+    @PostMapping("/user") // BindingResult를 사용하면 바로 에러가 나는 게 아니라 값이 들어오게 됨.
+    public ResponseEntity user(@Valid @RequestBody User user, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            bindingResult.getAllErrors().forEach(objectError -> {
+                FieldError field = (FieldError) objectError;
+                String message = objectError.getDefaultMessage();
+
+                System.out.println("field : " + field.getField());
+                System.out.println(message);
+
+                sb.append("field : " + field.getField());
+                sb.append("message : " + message);
+
+            });
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sb.toString());
+        }
+        System.out.println(user);
+        return ResponseEntity.ok(user);
+    }
+}
